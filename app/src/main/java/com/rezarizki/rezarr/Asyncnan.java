e6ffab08ef6ef18332c29c05c8fa62a577e685a3
dwikuntobayu <dwikunto.bayu@kiranatama.com>
2016-01-19 10:07:05 +0700

    Today i have learn about Asynctask
    -Task (ALL DONE)
    
    1. onPreExecute, position asynctask still in UI Thread, this process is handle initialize object that will process in background and create object that will show to parent activity.
    2. doInBackground, position asynctask in background Thread, in this process will handle process object item that was declare in onPreExecute, and if progress item was finish the result will publish with publishProgress.
    onProgressUpdate, position asynctask in UI Thread, this process will catch object that sent by doInBackground, after finish process this method will call back process doInBackground.
    3. onPostExecute, position asynctask in UI Thread, this process will run when all process in doInBackground was finished.

---
+++ app/src/main/java/com/rezarizki/rezarr/Asyncnan.java
@@ -0,0 +1,101 @@
+package com.rezarizki.rezarr;
+
+import android.app.Activity;
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+
+import java.util.ArrayList;
+
+public class Asyncnan extends AppCompatActivity {
+ListView name_list;
+    private String[] users = {
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state",
+            "name","address","age","city","state"
+    };
+    ProgressBar progress_bar;
+    AddArrayToListView arr_to_list_View;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_asyncnan);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        progress_bar = (ProgressBar)findViewById(R.id.progressBar);
+                progress_bar.setVisibility(View.VISIBLE);
+        name_list = (ListView)findViewById(R.id.listView);
+        name_list.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, new ArrayList<String>()));
+        arr_to_list_View = new AddArrayToListView();
+        arr_to_list_View.execute();
+
+    }
+class AddArrayToListView extends AsyncTask<Void, String, Void> {
+private  ArrayAdapter<String> adapter;
+    private int counter = 0;
+    ProgressDialog progress_dialog = new ProgressDialog(Asyncnan.this);
+
+
+    @Override
+    protected void onPreExecute() {
+        adapter = (ArrayAdapter<String>) name_list.getAdapter();
+        progress_dialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progress_dialog.setTitle("On Progress ....");
+        progress_dialog.setCancelable(false);
+        progress_dialog.setProgress(0);
+        progress_dialog.setButton(DialogInterface.BUTTON_NEGATIVE, "Cancel Process", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                arr_to_list_View.cancel(true);
+                progress_bar.setVisibility(View.INVISIBLE);
+                dialog.dismiss();
+            }
+        });
+        progress_dialog.show();
+    }
+@Override
+    protected Void doInBackground(Void...params) {
+    for (String item: users) {
+        publishProgress(item);
+        try {
+            Thread.sleep(100);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        if(isCancelled()) {
+            arr_to_list_View.cancel(true);
+        }
+    }
+    return null;
+}
+    @Override
+    protected void onProgressUpdate(String...values) {
+        adapter.add(values[0]);
+        counter ++;
+        Integer current_status = (int) ((counter / (float) users.length) * 100);
+        progress_bar.setProgress(current_status);
+        progress_dialog.setProgress(current_status);
+        progress_dialog.setMessage(String.valueOf(current_status)+ "%");
+    }
+                @Override
+    protected void onPostExecute(Void result)   {
+                    progress_bar.setVisibility(View.INVISIBLE);
+                    progress_dialog.dismiss();
+                }
+        }
+}
